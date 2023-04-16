ad1fe5e27ac0cddefe0c8febf048f66c87f1c775
gleb <glebtkach@rambler.ru>
2023-04-16 18:50:37 +0200

    created gateway with loadbalancing for beer microservices

---
+++ src/test/java/com/microservices/breweryapigateway/BreweryApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.breweryapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
